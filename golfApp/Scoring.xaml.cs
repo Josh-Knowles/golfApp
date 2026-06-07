e51b4d74d1a96829f2f157432f6a5422bf4a725d
Joshua Knowles <22044@bayfield-high.school.nz>
2026-06-07 22:13:22 +1200

    scoring

--- golfApp/Scoring.xaml.cs
+++ golfApp/Scoring.xaml.cs
@@ -11,12 +11,60 @@ public partial class Scoring : ContentPage
     private void entryHole1_Completed(object sender, EventArgs e)
     {
 		int scoreHole1 = int.Parse(entryHole1.Text);
-		lblTotalScore.Text = "Total Score: " + scoreHole1.ToString();
+		lblTotalScore.Text =  scoreHole1.ToString();
     }
 
     private void entryHole2_Completed(object sender, EventArgs e)
     {
 		int scoreHole2 = int.Parse(entryHole2.Text);
+        UpdateTotal(scoreHole2);
+        
 		
+    }
+	private void UpdateTotal(int holeScore)
+	{
+        int previousTotal = Convert.ToInt32(lblTotalScore.Text);
+        int totalScore = holeScore + previousTotal;
+        lblTotalScore.Text = totalScore.ToString();
+
+    }
+
+    private void entryHole3_Completed(object sender, EventArgs e)
+    {
+        int scoreHole3 = int.Parse(entryHole2.Text);
+        UpdateTotal(scoreHole3);
+    }
+
+    private void entryHole4_Completed(object sender, EventArgs e)
+    {
+        int scoreHole4 = int.Parse(entryHole2.Text);
+        UpdateTotal(scoreHole4);
+    }
+
+    private void entryHole5_Completed(object sender, EventArgs e)
+    {
+        int scoreHole5 = int.Parse(entryHole2.Text);
+        UpdateTotal(scoreHole5);
+    }
+
+    private void enrtyHole6_Completed(object sender, EventArgs e)
+    {
+        int scoreHole6 = int.Parse(entryHole2.Text);
+        UpdateTotal(scoreHole6);
+    }
+
+    private void entryHole7_Completed(object sender, EventArgs e)
+    {
+
+    }
+
+    private void entryHole8_Completed(object sender, EventArgs e)
+    {
+
+    }
+
+    private void entryHole9_Completed(object sender, EventArgs e)
+    {
+
     }
 }
