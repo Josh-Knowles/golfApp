7e857b06206f7ccc7afc4c36e5825e3f039566d6
Joshua Knowles <joshknowlesy@gmail.com>
2026-06-07 21:17:20 +1200

    7/6

--- golfApp/Scoring.xaml.cs
+++ golfApp/Scoring.xaml.cs
@@ -7,4 +7,16 @@ public partial class Scoring : ContentPage
 		InitializeComponent();
 		
 	}
+
+    private void entryHole1_Completed(object sender, EventArgs e)
+    {
+		int scoreHole1 = int.Parse(entryHole1.Text);
+		lblTotalScore.Text = "Total Score: " + scoreHole1.ToString();
+    }
+
+    private void entryHole2_Completed(object sender, EventArgs e)
+    {
+		int scoreHole2 = int.Parse(entryHole2.Text);
+		
+    }
 }
