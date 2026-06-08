9d6257c75fe80165525ea790e62fa60085fe7a0f
Joshua Knowles <joshknowlesy@gmail.com>
2026-06-08 12:56:44 +1200

    score9 holes fixed

--- golfApp/Scoring.xaml.cs
+++ golfApp/Scoring.xaml.cs
@@ -11,7 +11,7 @@ public partial class Scoring : ContentPage
     private void entryHole1_Completed(object sender, EventArgs e)
     {
 		int scoreHole1 = int.Parse(entryHole1.Text);
-		lblTotalScore.Text =  scoreHole1.ToString();
+		lblTotalScore.Text = "Total score: " + scoreHole1.ToString();
     }
 
     private void entryHole2_Completed(object sender, EventArgs e)
@@ -23,48 +23,53 @@ public partial class Scoring : ContentPage
     }
 	private void UpdateTotal(int holeScore)
 	{
-        int previousTotal = Convert.ToInt32(lblTotalScore.Text);
+        // gets the the total score from the label and reoves the text saying total score so it can be usd as an int in the calculation
+        int previousTotal = Convert.ToInt32(lblTotalScore.Text.Replace("Total score: ", ""));
         int totalScore = holeScore + previousTotal;
-        lblTotalScore.Text = totalScore.ToString();
+        lblTotalScore.Text = "Total score: " + totalScore.ToString();
+        holeScore = 0;
 
     }
 
     private void entryHole3_Completed(object sender, EventArgs e)
     {
-        int scoreHole3 = int.Parse(entryHole2.Text);
+        int scoreHole3 = Convert.ToInt32(entryHole3.Text);
         UpdateTotal(scoreHole3);
     }
 
     private void entryHole4_Completed(object sender, EventArgs e)
     {
-        int scoreHole4 = int.Parse(entryHole2.Text);
+        int scoreHole4 = Convert.ToInt32(entryHole4.Text);
         UpdateTotal(scoreHole4);
     }
 
     private void entryHole5_Completed(object sender, EventArgs e)
     {
-        int scoreHole5 = int.Parse(entryHole2.Text);
+        int scoreHole5 = Convert.ToInt32(entryHole5.Text);
         UpdateTotal(scoreHole5);
     }
 
-    private void enrtyHole6_Completed(object sender, EventArgs e)
+    private void entryHole6_Completed(object sender, EventArgs e)
     {
-        int scoreHole6 = int.Parse(entryHole2.Text);
+        int scoreHole6 = Convert.ToInt32(entryHole6.Text);
         UpdateTotal(scoreHole6);
     }
 
     private void entryHole7_Completed(object sender, EventArgs e)
     {
-
+        int scoreHole7 = Convert.ToInt32(entryHole7.Text);
+        UpdateTotal(scoreHole7);    
     }
 
     private void entryHole8_Completed(object sender, EventArgs e)
     {
-
+        int scoreHole8 = Convert.ToInt32(entryHole8.Text);
+        UpdateTotal(scoreHole8);
     }
 
     private void entryHole9_Completed(object sender, EventArgs e)
     {
-
+        int scoreHole9 = Convert.ToInt32(entryHole9.Text);
+        UpdateTotal(scoreHole9);
     }
 }
